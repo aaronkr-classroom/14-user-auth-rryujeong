bfc466d45fcf7c67e490e9ff1af7d8d36ef94d0d
rryujeong <126834727+rryujeong@users.noreply.github.com>
2024-06-12 06:23:20 +0000

    byeee

--- lesson-24/controllers/usersController.js
+++ lesson-24/controllers/usersController.js
@@ -6,7 +6,7 @@
  * userController.js에서 인덱스 액션 생성과 index 액션의 재방문
  */
 const passport = require("passport"),
-  User = require("../models/User.TODO"); // 사용자 모델 요청
+  User = require("../models/User"); // 사용자 모델 요청
 
 /**
  * Listing 22.3 (p. 328)
@@ -47,18 +47,25 @@ module.exports = {
    * usersController.js에서 passport 인증 미들웨어 추가
    * 원래 있는 코드는 다 지우고 아래 코드로 대체
    */
-
-  // local strategy로 사용자를 인증하기 위해 passport 호출
-  // authenticate: {...}  
-  // passport의 authenticate 메소드를 사용해 사용자 인증
+  authenticate: passport.authenticate("local", {
+    failureRedirect: "/users/login",
+    failureFlash: "Failed to login.",
+    successRedirect: "/",
+    successFlash: "Logged in!"
+  }),
 
   /**
-   * @TODO:
-   * 
    * Listing 24.8 (p. 359)
    * usersController.js에서 logout 액션 추가
    */
-  // logout: {...}
+  logout: (req, res, next) => {
+    req.logout(() => {
+      console.log("Logged out!");
+    }); // passport의 로그아웃 함수 실행
+    res.locals.redirect = "/";
+    req.flash("success", "Logged out!");
+    next();
+  },
 
   index: (req, res, next) => {
     User.find() // index 액션에서만 퀴리 실행
@@ -115,13 +122,20 @@ module.exports = {
     let newUser = new User(getUserParams(req.body)); // Listing 22.3 (p. 328)
 
     /**
-     * @TODO:
-     * 
      * Listing 24.4 (p. 355)
      * usersController.js에서 create 액션에서의 새로운 사용자 등록
      * 원래 있는 코드는 다 지우고 아래 코드로 대체
      */
-    // User.register();
+    User.register(newUser, req.body.password, (err, user) => {
+      if (user) {
+        res.locals.redirect = "/users";
+        req.flash("success", "Account created!");
+      } else {
+        res.locals.redirect = "/users/new";
+        req.flash("error", `Failed to create account! ${err.message}`);
+      }
+      next();
+    });
   },
 
   /**
